2e402d2729d5950daa9a7b48645a38ddde593072
nal27201@gmail.com <naing27201>
2024-02-24 16:20:09 +0630

    # fixed todo app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,72 +1,89 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Search from "./components/Search";
 import TodoList from "./components/TodoList";
 import Filter from "./components/Filter";
+import axios from "axios";
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 0, task: "Learn JavaScript", status: "Active" },
-    { id: 1, task: "Read a self-help book", status: "Active" },
-    { id: 2, task: "Play PS5", status: "Active" },
-    { id: 3, task: "Watch YouTube videos", status: "Active" },
-    // { id: 5, task: "Pray to God", status: "Active" },
-  ]);
+  const [todos, setTodos] = useState([]);
+  // const [errors, setErrors] = useState("");
 
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/todos").then((res) => setTodos(res.data));
+  });
 
-// add todo function
+  // add todo function
   const addTodo = (data) => {
-    setTodos( [ ...todos, data={...data, id:parseInt(todos[todos.length-1].id) + 1, status:"Active"}] )
-    console.log(data)
-  }
+    setTodos([
+      ...todos,
+      (data = {
+        ...data,
+        id: parseInt(todos[todos.length - 1].id) + 1,
+        status: "Active",
+      }),
+    ]);
+    axios
+      .post("http://127.0.0.1:8000/todos", data)
+      .then((res) => setTodos([...todos, res.data]));
+  };
 
   // delete function
   const delTodo = (id) => {
-    setTodos(todos.filter( todo => todo.id != id ))
-  }
-
+    setTodos(todos.filter((todo) => todo.id != id));
+    axios.delete("http://127.0.0.1:8000/todos/" + id);
+  };
 
   // update function
-  const updateTodo = (e, id, text) => {
-    e.preventDefault()
-    // this line helps to get the current todo based on the ID called todoId in TodoList
-    const todo = todos[id]
-    const updatedUser = { ...todo, task:text, status:"Active" }
-    setTodos(todos.map(t => t.id == todo.id ? updatedUser : t))
+  const updateTodo = (e, id, text, todo) => {
+    e.preventDefault();
+    const updateTodo = { ...todo, task: text };
+    console.log(id, "id.....");
+    axios.patch("http://127.0.0.1:8000/todos/" + id, updateTodo);
+  };
 
-  }
-
-  const completeTodo = (e, id) => {
-
-    if(e.target.checked){
-      console.log("okay")
-      setTodos(todos.map(todo => todo.id == id ? { ...todo, status:"Completed"}: todo))
+  const completeTodo = (e, id, todo) => {
+    if (e.target.checked) {
+      console.log("okay");
+      setTodos(
+        todos.map((todo) =>
+          todo.id == id ? { ...todo, completed: true } : todo
+        )
+      );
+      const updateTodo = { ...todo, completed: true };
+      console.log(id, "id.....");
+      axios.patch("http://127.0.0.1:8000/todos/" + id, updateTodo);
+    } else {
+      console.log("omo");
+      setTodos(
+        todos.map((todo) =>
+          todo.id == id ? { ...todo, completed: false } : todo
+        )
+      );
+      const updateTodo = { ...todo, completed: false };
+      console.log(id, "id.....");
+      axios.patch("http://127.0.0.1:8000/todos/" + id, updateTodo);
     }
-    else
-    {
-      console.log("omo")
-      setTodos(todos.map(todo => todo.id == id ? { ...todo, status:"Active"}: todo))
-    }
+  };
 
-   
-  }
-
-  const filterTodo = (cat_value) => {
-    // setTodos(todos.filter(todo => todo.status == cat_value))
-    setTodos(todos.filter((todo) => todo.status == cat_value))
-  }
-
+  // const filterTodo = (cat_value) => {
+  //   // setTodos(todos.filter(todo => todo.status == cat_value))
+  //   setTodos(todos.filter((todo) => todo.status == cat_value));
+  // };
 
   return (
     <div className="todo-container">
-      <Search addTodo = { addTodo } />
-      <Filter filter_todo = { filterTodo }/>
-      <TodoList todos = { todos } delTodo = { delTodo } update_todo = { updateTodo } complete_todo = { completeTodo } filter_todo = { filterTodo } />
+      <Search addTodo={addTodo} />
+      <Filter filter_todo={filterTodo} />
+      <TodoList
+        todos={todos}
+        delTodo={delTodo}
+        update_todo={updateTodo}
+        complete_todo={completeTodo}
+      />
     </div>
   );
 }
-
-
 
 export default App;
